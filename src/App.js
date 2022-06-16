939e2980e7d0ab45c21a17aaede82c6b267f4f1f
pranav s <dev.pranabsk@gmail.com>
2022-06-16 13:21:40 +0530

    Preparing project for Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import MainComponents from './components/MainComponents';
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <MainComponents />
+      </BrowserRouter>
      
     </div>
   );
