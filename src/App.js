841ba165c1dac98cb018bca5be5131b476ccfd12
pranav s <dev.pranabsk@gmail.com>
2022-07-08 11:28:16 +0530

    Creating store and connecting to React App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import MainComponents from './components/MainComponents';
 import { BrowserRouter } from "react-router-dom";
+import myStore from './redux/store';
+import {Provider} from 'react-redux';
 
 function App() {
   return (
     <div className="App">
+      <Provider store={myStore}>
       <BrowserRouter>
       <MainComponents />
       </BrowserRouter>
-     
+      </Provider>
     </div>
   );
 }
